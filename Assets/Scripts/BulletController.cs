b3f9911f242949ab5cf3ee0adadb6e14b5f3d1b3
Humberto Cristian Paez <crishpaez1993@gmail.com>
2021-10-26 00:21:05 -0300

    Desafio 4
    Shots with timer

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class BulletController : MonoBehaviour
 {
-    public float ftSpeedBullet= 0.00f;
+    public float ftSpeedBullet = 0.00f;
     public Vector3 direction;
     public int intDemage = 3;
+    public float ftDestroyBulletByTime = 0.2f;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Bullet Initial Frame");
         //Debug.DrawLine(transform.position, new Vector3(8, 8, -20), Color.red, 7f);
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoveBullet();
+        
+        ftDestroyBulletByTime -= Time.deltaTime;
+        if (ftDestroyBulletByTime > 0)
+        {
+            MoveBullet();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+                
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            ChangeScaleWithSpace();
+        }
+    }
+    private void ChangeScaleWithSpace() // Change scale with space
+    {
+        transform.localScale *= 2;
     }
 
     private void MoveBullet()
--- Assets/Scripts/CannonSpawner.cs
+++ Assets/Scripts/CannonSpawner.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,17 +7,21 @@ public class CannonSpawner : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public GameObject bulletSpawnPoint;
+    
+    public float ftIntervalTimeSpawnBullet =1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        SpawnBullet();
+        InvokeRepeating("SpawnBullet", 3f, ftIntervalTimeSpawnBullet);        
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
+      
+
     void SpawnBullet()
     {
         Instantiate(bulletPrefab, bulletSpawnPoint.transform.position, transform.rotation);
